5bcf1ef691165aca0c27f8a3f842a3313a3561ca
emonkhan500 <khdilo47@gmail.com>
2024-04-28 21:49:58 +0600

    gitignore file add

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hzfjxhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
